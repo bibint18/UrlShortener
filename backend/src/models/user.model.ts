d91778b98e253270fbd0cad45062c4c8979b1c6f
Bibin T <bibint2918@gmail.com>
2025-05-26 00:20:04 +0530

    Changed password field for google login

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -4,7 +4,7 @@ import { Iuser } from "../interfaces/models/user.interface.js";
 const UserSchema = new Schema<Iuser>({
   fullName:{type:String,required:true},
   email:{type:String,required:true,unique:true},
-  hashedPassword:{type:String,required:true},
+  hashedPassword:{type:String},
   isVerified:{type:Boolean,default:false},
   refreshToken:{type:String},
   createdAt:{type:Date,default:Date.now()},
